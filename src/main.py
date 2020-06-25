43a5a3ef54373e701feedc9bb337fe247c79c265
joaoaraujo72 <up201703919@fc.up.pt>
2020-06-25 15:54:55 +0100

    Add ADC

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 # pylint: disable=global-statement, unused-argument
 from MicroWebSrv2 import MicroWebSrv2
-from machine import Pin
+from machine import Pin, ADC
+from utime import ticks_ms
+from api import getTime
 
+adc = ADC(Pin(32))
 but = Pin(23, Pin.IN, Pin.PULL_UP)
+Readings = []
+adc.atten(ADC.ATTN_11DB)
+adc.width(ADC.WIDTH_12BIT)
 
 myWebSockets = None
 
@@ -15,7 +21,6 @@ def OnWebSocketAccepted(microWebSrv2, webSocket):
   if myWebSockets is None:
     print('WebSocket from {0}'.format(webSocket.Request.UserAddress))
     myWebSockets = webSocket
-    myWebSockets.OnTextMessage = OnWebSocketTextMsg
     myWebSockets.OnClosed = OnWebSocketClosed
 
 mws2 = MicroWebSrv2()
@@ -27,15 +32,24 @@ mws2.StartManaged()
 
 state = True
 def loop():
-  global state
-  if but.value() != state:
-    state = not state
-    msg = 'Button ' + ('OFF' if state else 'ON')
-    print(msg)
+  global last, Readings
+  now = ticks_ms()
+  if now - last >= 3000:
+    last = now
+    value = 0
+    for n in range(16):
+      value = value + adc.read()
+    value = value >> 4
+    Volts = (value * 3.3) / 4095
+    Point = []
+    Point.append(getTime())
+    Point.append(Volts)
+    Readings.append(Point)
     if myWebSockets is not None:
-      myWebSockets.SendTextMessage(msg)
+      myWebSockets.SendTextMessage(str(Readings))
 
 try:
+  last = ticks_ms()
   while mws2.IsRunning:
     loop()
 except KeyboardInterrupt:
