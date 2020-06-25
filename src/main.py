bd000f1bdbd7d05519434e57a5c73eb9d2975e45
joaoaraujo72 <up201703919@fc.up.pt>
2020-06-25 14:41:09 +0100

    create websocket

--- src/main.py
+++ src/main.py
@@ -1,1 +1,44 @@
-# main.py
+# pylint: disable=global-statement, unused-argument
+from MicroWebSrv2 import MicroWebSrv2
+from machine import Pin
+
+but = Pin(23, Pin.IN, Pin.PULL_UP)
+
+myWebSockets = None
+
+def OnWebSocketClosed(webSocket):
+  global myWebSockets
+  myWebSockets = None
+
+def OnWebSocketAccepted(microWebSrv2, webSocket):
+  global myWebSockets
+  if myWebSockets is None:
+    print('WebSocket from {0}'.format(webSocket.Request.UserAddress))
+    myWebSockets = webSocket
+    myWebSockets.OnTextMessage = OnWebSocketTextMsg
+    myWebSockets.OnClosed = OnWebSocketClosed
+
+mws2 = MicroWebSrv2()
+wsMod = MicroWebSrv2.LoadModule('WebSockets')
+wsMod.OnWebSocketAccepted = OnWebSocketAccepted
+mws2.SetEmbeddedConfig()
+mws2.NotFoundURL = '/'
+mws2.StartManaged()
+
+state = True
+def loop():
+  global state
+  if but.value() != state:
+    state = not state
+    msg = 'Button ' + ('OFF' if state else 'ON')
+    print(msg)
+    if myWebSockets is not None:
+      myWebSockets.SendTextMessage(msg)
+
+try:
+  while mws2.IsRunning:
+    loop()
+except KeyboardInterrupt:
+  pass
+
+mws2.Stop()
